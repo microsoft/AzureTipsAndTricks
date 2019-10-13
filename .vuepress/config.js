5b97f3c7e2560cb14f8937b24a6dd8dfc0a8f47f
Michael Crump <mbcrump29@gmail.com>
2019-10-13 15:41:55 -0700

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -32,7 +32,7 @@ module.exports = {
 			{
 				title: 'Recently Added',
 				collapsable: false,
-				children: ['/blog/tip228','/blog/tip227','/blog/tip226','/blog/tip225','/blog/tip224','/blog/tip223','/blog/tip222','/blog/tip221','/blog/tip220','/blog/tip219','/blog/tip218','/blog/tip217','/blog/tip216','/blog/tip215','/blog/tip214','/blog/tip213','/blog/tip212','/blog/tip211','/blog/tip210']
+				children: ['/blog/tip229','/blog/tip228','/blog/tip227','/blog/tip226','/blog/tip225','/blog/tip224','/blog/tip223','/blog/tip222','/blog/tip221','/blog/tip220','/blog/tip219','/blog/tip218','/blog/tip217','/blog/tip216','/blog/tip215','/blog/tip214','/blog/tip213','/blog/tip212','/blog/tip211','/blog/tip210']
 			},
 			{
 				title: 'App Service',
@@ -131,7 +131,7 @@ module.exports = {
 			{
 				title: 'Kubernetes',
 				collapsable: true,
-				children: ['/blog/tip228']
+				children: ['/blog/tip229','/blog/tip228']
 			},
 			{
 				title: 'Logic Apps',
