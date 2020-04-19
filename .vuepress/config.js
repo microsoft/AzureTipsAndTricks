a358c76abaa3a72ed9099ab5dfc73a7ed48cbd45
Michael Crump <mbcrump29@gmail.com>
2020-04-19 09:01:22 -0700

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -32,7 +32,7 @@ module.exports = {
 			{
 				title: 'Recently Added',
 				collapsable: false,
-children: ['/blog/tip259','/blog/tip258','/blog/tip257','/blog/tip256']
+children: ['/blog/tip260','/blog/tip258','/blog/tip257','/blog/tip256']
 			},
 			{
 				title: '.NET',
@@ -161,7 +161,7 @@ children: ['/blog/tip259','/blog/tip258','/blog/tip257','/blog/tip256']
 			{
 				title: 'Functions',
 				collapsable: true,
-				children: ['/blog/tip247','/blog/tip211','/blog/tip196','/blog/tip35', '/blog/tip36', '/blog/tip50', '/blog/tip51', '/blog/tip52', '/blog/tip61', '/blog/tip62', '/blog/tip63', '/blog/tip64', '/blog/tip94', '/blog/tip97', '/blog/tip98', '/blog/tip99', '/blog/tip100', '/blog/tip130', '/blog/tip133', '/blog/tip134', '/blog/tip135', '/blog/tip136', '/blog/tip147', '/blog/tip148', '/blog/tip157', '/blog/tip158', '/blog/tip161','/blog/tip125','/blog/tip174', '/blog/tip175']
+				children: ['/blog/tip260','/blog/tip247','/blog/tip211','/blog/tip196','/blog/tip35', '/blog/tip36', '/blog/tip50', '/blog/tip51', '/blog/tip52', '/blog/tip61', '/blog/tip62', '/blog/tip63', '/blog/tip64', '/blog/tip94', '/blog/tip97', '/blog/tip98', '/blog/tip99', '/blog/tip100', '/blog/tip130', '/blog/tip133', '/blog/tip134', '/blog/tip135', '/blog/tip136', '/blog/tip147', '/blog/tip148', '/blog/tip157', '/blog/tip158', '/blog/tip161','/blog/tip125','/blog/tip174', '/blog/tip175']
 			},
 			{
 				title: 'GitHub',
