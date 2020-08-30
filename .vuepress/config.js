b4caf5827ff8520af6b27c032cf51b104e2ae7e0
Michael Crump <mbcrump29@gmail.com>
2020-08-30 11:19:15 -0700

    deploy

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -32,7 +32,7 @@ module.exports = {
 			{
 				title: 'Recently Added',
 				collapsable: false,
-children: ['/blog/tip275','/blog/tip274','/blog/tip273','/blog/tip272','/blog/tip271','/blog/tip270','/blog/tip269','/blog/tip268','/blog/tip267','/blog/tip266','/blog/tip265','/blog/tip264','/blog/tip262']
+children: ['/blog/tip276','/blog/tip275','/blog/tip274','/blog/tip273','/blog/tip272','/blog/tip271','/blog/tip270','/blog/tip269','/blog/tip268','/blog/tip267','/blog/tip266','/blog/tip265','/blog/tip264','/blog/tip262']
 			},
 			{
 				title: '.NET',
